392505238ba611a78c8bdac6fdc32cd5de59bf70
Kancabricot <timothee.fontan@gmail.com>
2022-11-14 11:15:03 +0100

    Ajout Menu et régulation de probleme

---
+++ Assets/Script/MenuStart/MenuButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public void FishTheKingButton()
+    {
+        SceneManager.LoadScene("FishTheKing");
+    }
+
+    public void DrawButton()
+    {
+        SceneManager.LoadScene("MiniJeuDessin");
+    }
+
+}
