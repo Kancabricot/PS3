eb64f7a6fd172de7a0db11afa51f4d1932ab24a5
Kancabricot <timothee.fontan@gmail.com>
2022-11-30 14:07:38 +0100

    Push feedBack et animation

--- Assets/Script/MenuStart/SlectionLevelButton.cs
+++ Assets/Script/MenuStart/SlectionLevelButton.cs
@@ -9,6 +9,17 @@ public class SlectionLevelButton : MonoBehaviour
 {
     [SerializeField] GameObject group;
     [SerializeField] GameObject WIP;
+
+    [SerializeField] GameObject statue;
+    [SerializeField] GameObject arcDeTriumphe;
+    [SerializeField] GameObject aqueduc;
+    [SerializeField] GameObject chateauDEau;
+
+    [SerializeField] GameObject title;
+    [SerializeField] GameObject right;
+    [SerializeField] GameObject left;
+    [SerializeField] GameObject play;
+
     public static int IndexChoice = 1;
 
     public void GoRight()
@@ -16,6 +27,10 @@ public class SlectionLevelButton : MonoBehaviour
         if(group.transform.eulerAngles.y %45 == 0)
         {
             IndexChoice += 1;
+            if(IndexChoice == 5)
+            {
+                IndexChoice = 1;
+            }
             group.transform.DORotate(new Vector3(0, group.transform.eulerAngles.y + 90, 0), 1.5f, RotateMode.FastBeyond360)
             .SetDelay(0.5f)
             .SetEase(Ease.OutBack);
@@ -37,7 +52,35 @@ public class SlectionLevelButton : MonoBehaviour
     {
         if(IndexChoice == 1)
         {
-            SceneManager.LoadScene("MiniJeuDessin");
+            statue.transform.DORotate(new Vector3(0, 1980, 0), 1.5f, RotateMode.FastBeyond360)
+            .SetDelay(0.5f)
+            .SetEase(Ease.InQuad);
+
+            statue.transform.DOScale(new Vector3(0, 0, 0), 1.5f)
+            .SetDelay(0.5f)
+            .SetEase(Ease.InQuad);
+
+            arcDeTriumphe.transform.DOScale(new Vector3(0, 0, 0), 1f);
+
+            aqueduc.transform.DOMoveX(100, 0.5f, false)
+                    .SetEase(Ease.OutQuad);
+
+            chateauDEau.transform.DOMoveX(-100, 0.5f, false)
+                    .SetEase(Ease.OutQuad);
+
+            title.transform.DOMoveY(100, 0.5f, false)
+                    .SetEase(Ease.OutQuad);
+
+            play.transform.DOMoveY(-100, 0.5f, false)
+                    .SetEase(Ease.OutQuad);
+
+            right.transform.DOMoveX(100, 0.5f, false)
+                    .SetEase(Ease.OutQuad);
+
+            left.transform.DOMoveX(-100, 0.5f, false)
+                    .SetEase(Ease.OutQuad);
+
+            Invoke("StartGame", 2f);
         }
         else
         {
@@ -51,5 +94,10 @@ public class SlectionLevelButton : MonoBehaviour
     {
         WIP.transform.DOMoveY(transform.position.y - 20, 0.5f, false)
                     .SetEase(Ease.OutQuad);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("MiniJeuDessin");
     }
 }
