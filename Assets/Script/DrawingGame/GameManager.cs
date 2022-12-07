c3aa380e8f2c337a99a9b4fbc04197f2ad2bab90
Kancabricot <timothee.fontan@gmail.com>
2022-12-07 17:47:44 +0100

    Je ne sais plus ou j'en suis mais is ok dans une heure je saurai

--- Assets/Script/DrawingGame/GameManager.cs
+++ Assets/Script/DrawingGame/GameManager.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject[] attach;
     [SerializeField] GameObject[] linetrace;
     [SerializeField] GameObject winMenu;
+    [SerializeField] Image bar;
+    [SerializeField] float maxDistance;
 
 
     private bool fingerDown = false;
+
+
+    private void Start()
+    {
+        attach = GameObject.FindGameObjectsWithTag("Attach");
+        maxDistance = attach.Length;
+    }
 
     private void OnMouseUp()
     {
@@ -34,5 +44,7 @@ public class GameManager : MonoBehaviour
     {
         attach = GameObject.FindGameObjectsWithTag("Attach");
         linetrace = GameObject.FindGameObjectsWithTag("Line");
+
+        bar.fillAmount = attach.Length / maxDistance;
     }
 }
