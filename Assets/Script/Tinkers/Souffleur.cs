94be4a8f3f630f2616a84ee70c5ccf56d5f465a4
Kancabricot <timothee.fontan@gmail.com>
2022-12-08 17:01:39 +0100

    Fx + Background tinker

--- Assets/Script/Tinkers/Souffleur.cs
+++ Assets/Script/Tinkers/Souffleur.cs
@@ -8,16 +8,19 @@ public class Souffleur : MonoBehaviour
     [SerializeField] GameObject termo;
     [SerializeField] GameObject mold;
     [SerializeField] GameObject bar;
+    [SerializeField] SpriteRenderer soufflet;
+    [SerializeField] Sprite souffletUp;
+    [SerializeField] Sprite souffletDown;
 
     private void OnMouseDown()
     {
-        transform.localScale = new Vector3(1, 0.2f, 1);
+        soufflet.sprite = souffletDown;
         FindObjectOfType<Cauldron>().IncreaseHeat();
     }
 
     private void OnMouseUp()
     {
-        transform.localScale = new Vector3(1, 0.7f, 1);
+        soufflet.sprite = souffletUp;
     }
 
     public void ExitMesh()
